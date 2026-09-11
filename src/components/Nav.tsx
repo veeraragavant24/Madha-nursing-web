b3d7b0f4229416fb580424981e8db9b3fccada93
veeraragavant24 <veeraragavant861@gmail.com>
2026-09-11 16:43:40 +0530

    edit in nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1454,7 +1454,7 @@ padding-left: clamp(10px, 1vw, 18px) !important;
   left: 50% !important;
   right: auto !important;
 
-  top: 18px !important;
+  top: 20px !important;
 
   width: calc(100% - 20px) !important;
   max-width: none !important;
